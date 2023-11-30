904fcc3291bfbdd696f3e7ba899a1bd48c9cbf3a
yut23 <yut23@gvljohnsons.com>
2023-11-30 18:08:57 -0500

    Fix slow autoenv.zsh

--- 2022/.autoenv.zsh
+++ 2022/.autoenv.zsh
@@ -1,4 +1,8 @@
-autostash alias workon=${0:h:h}/tools/cpp/workon
-autostash alias aoc=${0:h:h}/tools/cpp/aoc
-autostash alias iwyu=${0:h:h}/tools/cpp/iwyu
-autostash alias profile=${0:h:h}/tools/cpp/profile.sh
+if [[ $autoenv_event == 'enter' ]]; then
+  alias workon=${0:h:h}/tools/cpp/workon
+  alias aoc=${0:h:h}/tools/cpp/aoc
+  alias iwyu=${0:h:h}/tools/cpp/iwyu
+  alias profile=${0:h:h}/tools/cpp/profile.sh
+else
+  unalias workon aoc iwyu profile
+fi
