fc91a3dcccc979bda6026bbbf07de499ab57e02a
yut23 <yut23@gvljohnsons.com>
2023-12-01 02:11:59 -0500

    2023: day 1 part 2 complete

--- 2023/src/day01.cpp
+++ 2023/src/day01.cpp
@@ -7,31 +7,68 @@
 
 #include "lib.hpp"
 #include <iostream> // for cout
-#include <numeric>  // for reduce
+#include <iterator> // for distance
+#include <ranges>   // for find
 #include <regex>    // for regex, sregex_iterator
 #include <string>   // for string, getline
 #include <vector>   // for vector
 
+const std::vector<std::string> digit_names{
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+int parse_digit(std::string match, const bool reverse = false) {
+    if (match.length() == 1) {
+        return match[0] - '0';
+    } else {
+        if (reverse) {
+            match = std::string{match.rbegin(), match.rend()};
+        }
+        return std::distance(digit_names.begin(),
+                             std::ranges::find(digit_names, match)) +
+               1;
+    }
+}
+
 int main(int argc, char **argv) {
     std::ifstream infile = aoc::parse_args(argc, argv);
 
-    const std::regex digit_regex{"\\d"};
+    // part 1
+    const std::string digits{"123456789"};
 
     // read file line-by-line
     std::string line;
     int total = 0;
     while (std::getline(infile, line)) {
-        auto digit_iter =
-            std::sregex_iterator(line.cbegin(), line.cend(), digit_regex);
-        auto digit_end = std::sregex_iterator();
-        int first_digit = (digit_iter++)->str()[0] - '0';
-        int last_digit = first_digit;
-        for (; digit_iter != digit_end; ++digit_iter) {
-            last_digit = digit_iter->str()[0] - '0';
-        }
+        int first_digit = line[line.find_first_of(digits)] - '0';
+        int last_digit = line[line.find_last_of(digits)] - '0';
         total += 10 * first_digit + last_digit;
     }
 
     std::cout << total << "\n";
+
+    // part 2
+    // reset infile
+    infile.clear();
+    infile.seekg(0, std::ios::beg);
+
+    const std::regex digit_regex{
+        "[1-9]|one|two|three|four|five|six|seven|eight|nine"};
+    const std::regex rev_digit_regex{
+        "[1-9]|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno"};
+
+    total = 0;
+    while (std::getline(infile, line)) {
+        auto digit_iter =
+            std::sregex_iterator(line.cbegin(), line.cend(), digit_regex);
+        int first_digit = parse_digit(digit_iter->str());
+        std::string rev_line{line.rbegin(), line.rend()};
+        auto rev_digit_iter = std::sregex_iterator(
+            rev_line.cbegin(), rev_line.cend(), rev_digit_regex);
+        int last_digit = parse_digit(rev_digit_iter->str(), true);
+        total += 10 * first_digit + last_digit;
+    }
+
+    std::cout << total << "\n";
+
     return 0;
 }
