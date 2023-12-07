327e4efb3013c3cc319a6db8b7e8cd0cfb3447c4
yut23 <yut23@gvljohnsons.com>
2023-12-07 13:26:44 -0500

    Add Github workflow to run unit tests
    Also re-run everything if the workflow scripts are modified

--- .github/workflows/gen_matrix.py
+++ .github/workflows/gen_matrix.py
@@ -20,22 +20,31 @@ def add_config(directory: str, target: str) -> None:
 
 
 def main() -> None:
-    changed_files = json.loads(sys.argv[1])
+    target_glob = sys.argv[1]
+    changed_files = json.loads(sys.argv[2])
     for file in changed_files:
         path = Path(file)
+        if path.parts[:2] == (".github", "workflows"):
+            for makefile in Path().glob("*/Makefile"):
+                directory = makefile.parent.name
+                for path in (Path(directory) / "src").glob(target_glob):
+                    add_config(directory=directory, target=path.stem)
+            continue
         directory = path.parts[0]
-        if path.name in {"Makefile", "lib.h", "lib.hpp"}:
-            for path in (Path(directory) / "src").glob("day*.cpp"):
+        if path.name in {"Makefile", "lib.h", "lib.hpp"} or "unit_test" in path.parts:
+            for path in (Path(directory) / "src").glob(target_glob):
                 add_config(directory=directory, target=path.stem)
         else:
-            assert path.stem.startswith("day") and path.suffix == ".cpp"
+            assert (
+                path.stem.startswith("day") or path.stem.startswith("test")
+            ) and path.suffix in {".cpp", ".hpp"}
             add_config(directory=directory, target=path.stem)
 
     if configs:
         matrix = {"include": list(map(asdict, sorted(configs)))}
     else:
         matrix = {}
-    with open(sys.argv[2], "a") as f:
+    with open(sys.argv[3], "a") as f:
         f.write("matrix-combinations=")
         json.dump(matrix, f, indent=None, separators=(",", ":"))
         f.write("\n")
