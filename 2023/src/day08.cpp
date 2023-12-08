223d13cb3357900308824a97a523c694f89d9b07
yut23 <yut23@gvljohnsons.com>
2023-12-08 16:36:13 -0500

    2023: day 8 part 2 (mostly) complete
    Part 2 fails on the examples

--- 2023/src/day08.cpp
+++ 2023/src/day08.cpp
@@ -5,45 +5,19 @@
  * Created:     2023-12-08
  *****************************************************************************/
 
-#include "lib.hpp"
+#include "day08.hpp"
+#include "lib.hpp"  // for parse_args, DEBUG
+#include <cassert>  // for assert
 #include <iostream> // for cout
-#include <map>      // for map
-#include <memory>   // for unique_ptr, make_unique
+#include <numeric>  // for lcm
 #include <string>   // for string
-#include <vector>   // for vector
-
-namespace aoc::day08 {
-struct Node {
-    std::string id;
-    std::string left;
-    std::string right;
-
-    Node(const std::string &id, const std::string &left,
-         const std::string &right)
-        : id(id), left(left), right(right) {}
-};
-
-using Network = std::map<std::string, std::unique_ptr<Node>>;
-
-Network read_maps(std::istream &is) {
-    Network network;
-    std::string id, left, right;
-    while (is >> id >> aoc::skip(1) >> left >> right) {
-        left = left.substr(1, 3);
-        right.resize(3);
-        network.try_emplace(id, std::make_unique<Node>(id, left, right));
-    }
-    return network;
-}
-} // namespace aoc::day08
 
 int part_1(const std::string &directions, const aoc::day08::Network &network) {
     int steps = 0;
-    aoc::day08::Node *curr = network.find("AAA")->second.get();
+    const aoc::day08::Node *curr = network.get("AAA");
     auto dir_it = directions.begin();
     while (curr->id != "ZZZ") {
-        curr = network.find(*dir_it == 'L' ? curr->left : curr->right)
-                   ->second.get();
+        curr = network.follow(curr, *dir_it);
         // repeat the direction string if we hit the end
         if (++dir_it == directions.end()) {
             dir_it = directions.begin();
@@ -53,35 +27,18 @@ int part_1(const std::string &directions, const aoc::day08::Network &network) {
     return steps;
 }
 
-int part_2(const std::string &directions, const aoc::day08::Network &network) {
-    // let's see brute force is feasible...
-    std::vector<aoc::day08::Node *> curr_nodes;
-    for (const auto &[id, node_ptr] : network) {
-        if (id[2] == 'A') {
-            curr_nodes.push_back(node_ptr.get());
+long part_2(const std::string &directions, const aoc::day08::Network &network) {
+    auto cycles = aoc::day08::find_cycles(directions, network);
+    long multiple = 1;
+    // TODO: this works for the real input, but not for the examples.
+    for (const auto &cycle : cycles) {
+        if constexpr (aoc::DEBUG) {
+            std::cerr << cycle << "\n";
         }
+        assert(cycle.start == cycle.length);
+        multiple = std::lcm(multiple, cycle.length);
     }
-    int steps = 0;
-    auto dir_it = directions.begin();
-    while (true) {
-        bool all_z = true;
-        for (const auto node : curr_nodes) {
-            all_z &= node->id[2] == 'Z';
-        }
-        if (all_z) {
-            break;
-        }
-        for (auto &node : curr_nodes) {
-            node = network.find(*dir_it == 'L' ? node->left : node->right)
-                       ->second.get();
-        }
-        // repeat the direction string if we hit the end
-        if (++dir_it == directions.end()) {
-            dir_it = directions.begin();
-        }
-        ++steps;
-    }
-    return steps;
+    return multiple;
 }
 
 int main(int argc, char **argv) {
@@ -91,7 +48,7 @@ int main(int argc, char **argv) {
     infile >> directions;
     auto network = aoc::day08::read_maps(infile);
 
-    // std::cout << part_1(directions, network) << "\n";
+    std::cout << part_1(directions, network) << "\n";
     std::cout << part_2(directions, network) << "\n";
 
     return 0;
---
+++ 2023/src/day08.hpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * File:        day08.hpp
+ *
+ * Author:      Eric T. Johnson (yut23)
+ * Created:     2023-12-08
+ *****************************************************************************/
+
+#include "lib.hpp"   // for skip
+#include <algorithm> // for sort
+#include <iostream>  // for cout
+#include <map>       // for map
+#include <memory>    // for unique_ptr, make_unique
+#include <string>    // for string
+#include <utility>   // for pair, make_pair, move
+#include <vector>    // for vector
+
+namespace aoc::day08 {
+
+struct Node {
+    std::string id;
+    std::string left;
+    std::string right;
+
+    Node(const std::string &id, const std::string &left,
+         const std::string &right)
+        : id(id), left(left), right(right) {}
+};
+
+struct Network {
+    std::map<std::string, std::unique_ptr<Node>> nodes;
+
+    void add_node(const std::string &id, const std::string &left,
+                  const std::string &right) {
+        nodes.try_emplace(id, std::make_unique<Node>(id, left, right));
+    }
+
+    const Node *follow(const Node *node, char direction) const {
+        return nodes.at(direction == 'L' ? node->left : node->right).get();
+    }
+    const Node *get(const std::string &id) const { return nodes.at(id).get(); }
+};
+
+Network read_maps(std::istream &is) {
+    Network network;
+    std::string id, left, right;
+    while (is >> id >> aoc::skip(1) >> left >> right) {
+        left = left.substr(1, 3);
+        right.resize(3);
+        network.add_node(id, left, right);
+    }
+    return network;
+}
+
+struct FinishEntry {
+    long step;
+    std::string node_id;
+
+    FinishEntry(long step, const std::string &node_id)
+        : step(step), node_id(node_id) {}
+
+    auto operator<=>(const FinishEntry &) const = default;
+};
+
+struct CycleInfo {
+    std::string start_id;
+    long start;
+    long length;
+    std::vector<FinishEntry> entries;
+
+    CycleInfo(const std::string &start_id, long start, long length,
+              const std::vector<FinishEntry> &entries)
+        : start_id(start_id), start(start), length(length), entries(entries) {}
+    CycleInfo(const std::string &start_id, long start, long length,
+              std::vector<FinishEntry> &&entries)
+        : start_id(start_id), start(start), length(length),
+          entries(std::move(entries)) {}
+};
+
+std::ostream &operator<<(std::ostream &os, const CycleInfo &cycle) {
+    os << "CycleInfo(start_id=" << cycle.start_id << ", start=" << cycle.start
+       << ", length=" << cycle.length << ", entries={";
+    for (auto it = cycle.entries.begin(); it != cycle.entries.end(); ++it) {
+        if (it != cycle.entries.begin()) {
+            os << ", ";
+        }
+        os << it->step << ": " << it->node_id;
+    }
+    os << "})";
+    return os;
+}
+
+std::vector<CycleInfo> find_cycles(const std::string &directions,
+                                   const aoc::day08::Network &network) {
+    std::vector<const aoc::day08::Node *> curr_nodes;
+    for (const auto &[id, node_ptr] : network.nodes) {
+        if (id[2] == 'A') {
+            curr_nodes.push_back(node_ptr.get());
+        }
+    }
+    std::vector<aoc::day08::CycleInfo> cycles;
+    for (auto node : curr_nodes) {
+        std::string start_id = node->id;
+        std::map<std::pair<std::string, int>, FinishEntry> z_hits{};
+        long step = 0;
+        int dir_idx = 0;
+        while (true) {
+            if (node->id[2] == 'Z') {
+                auto key = std::make_pair(node->id, dir_idx);
+                auto it = z_hits.find(key);
+                if (it != z_hits.end()) {
+                    // found a cycle!
+                    int start = it->second.step;
+                    std::vector<FinishEntry> entries;
+                    for (auto &[_, entry] : z_hits) {
+                        entries.push_back(std::move(entry));
+                    }
+                    std::ranges::sort(entries);
+                    cycles.emplace_back(start_id, start, step - start, entries);
+                    break;
+                } else {
+                    z_hits.try_emplace(key, FinishEntry{step, node->id});
+                }
+            }
+            node = network.follow(node, directions[dir_idx]);
+            // repeat the direction string if we hit the end
+            dir_idx = (dir_idx + 1) % directions.size();
+            ++step;
+        }
+    }
+    return cycles;
+}
+
+} // namespace aoc::day08
