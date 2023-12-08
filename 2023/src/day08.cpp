129196c5ad11400014c6b426e266af68b1a89b80
yut23 <yut23@gvljohnsons.com>
2023-12-08 00:39:16 -0500

    2023 wip: day 8 part 2 brute force

--- 2023/src/day08.cpp
+++ 2023/src/day08.cpp
@@ -10,6 +10,7 @@
 #include <map>      // for map
 #include <memory>   // for unique_ptr, make_unique
 #include <string>   // for string
+#include <vector>   // for vector
 
 namespace aoc::day08 {
 struct Node {
@@ -52,6 +53,37 @@ int part_1(const std::string &directions, const aoc::day08::Network &network) {
     return steps;
 }
 
+int part_2(const std::string &directions, const aoc::day08::Network &network) {
+    // let's see brute force is feasible...
+    std::vector<aoc::day08::Node *> curr_nodes;
+    for (const auto &[id, node_ptr] : network) {
+        if (id[2] == 'A') {
+            curr_nodes.push_back(node_ptr.get());
+        }
+    }
+    int steps = 0;
+    auto dir_it = directions.begin();
+    while (true) {
+        bool all_z = true;
+        for (const auto node : curr_nodes) {
+            all_z &= node->id[2] == 'Z';
+        }
+        if (all_z) {
+            break;
+        }
+        for (auto &node : curr_nodes) {
+            node = network.find(*dir_it == 'L' ? node->left : node->right)
+                       ->second.get();
+        }
+        // repeat the direction string if we hit the end
+        if (++dir_it == directions.end()) {
+            dir_it = directions.begin();
+        }
+        ++steps;
+    }
+    return steps;
+}
+
 int main(int argc, char **argv) {
     std::ifstream infile = aoc::parse_args(argc, argv);
 
@@ -59,7 +91,8 @@ int main(int argc, char **argv) {
     infile >> directions;
     auto network = aoc::day08::read_maps(infile);
 
-    std::cout << part_1(directions, network) << "\n";
+    // std::cout << part_1(directions, network) << "\n";
+    std::cout << part_2(directions, network) << "\n";
 
     return 0;
 }
