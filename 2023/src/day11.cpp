78c50cb33484cf51b67279b80c891434b253e53e
yut23 <yut23@gvljohnsons.com>
2023-12-11 18:22:20 -0800

    2023: day 11 part 2 complete

--- 2023/src/day11.cpp
+++ 2023/src/day11.cpp
@@ -10,21 +10,8 @@
 #include <cstddef>  // for size_t
 #include <iostream> // for cout, cerr
 
-int part_1(std::vector<aoc::Pos> galaxies) {
-    if constexpr (aoc::DEBUG) {
-        std::cerr << "before expanding:\n";
-        for (const auto &pos : galaxies) {
-            std::cerr << "  " << pos << "\n";
-        }
-    }
-    aoc::day11::expand(galaxies);
-    if constexpr (aoc::DEBUG) {
-        std::cerr << "\nafter expanding:\n";
-        for (const auto &pos : galaxies) {
-            std::cerr << "  " << pos << "\n";
-        }
-    }
-    int path_sum = 0;
+long sum_paths(const std::vector<aoc::Pos> &galaxies) {
+    long path_sum = 0;
     for (std::size_t i = 0; i < galaxies.size(); ++i) {
         for (std::size_t j = i + 1; j < galaxies.size(); ++j) {
             path_sum += (galaxies[i] - galaxies[j]).manhattan_distance();
@@ -38,7 +25,12 @@ int main(int argc, char **argv) {
 
     std::vector<aoc::Pos> galaxies = aoc::day11::read_image(infile);
 
-    std::cout << part_1(galaxies) << "\n";
+    std::cout << sum_paths(aoc::day11::expand(galaxies, 2)) << "\n";
+    if constexpr (aoc::DEBUG) {
+        std::cout << sum_paths(aoc::day11::expand(galaxies, 10)) << "\n";
+        std::cout << sum_paths(aoc::day11::expand(galaxies, 100)) << "\n";
+    }
+    std::cout << sum_paths(aoc::day11::expand(galaxies, 1000000)) << "\n";
 
     return 0;
 }
--- 2023/src/day11.hpp
+++ 2023/src/day11.hpp
@@ -5,15 +5,16 @@
  * Created:     2023-12-11
  *****************************************************************************/
 
-#include "lib.hpp"  // for Pos
-#include <iostream> // for istream
-#include <set>      // for set
-#include <string>   // for string, getline
-#include <vector>   // for vector
+#include "lib.hpp"   // for Pos
+#include <algorithm> // for transform
+#include <iostream>  // for istream
+#include <set>       // for set
+#include <string>    // for string, getline
+#include <vector>    // for vector
 
 namespace aoc::day11 {
 
-void expand(std::vector<Pos> &galaxies) {
+std::vector<Pos> expand(const std::vector<Pos> &galaxies, int amount) {
     // enumerate all rows and columns containing galaxies
     std::set<int> nonempty_columns;
     std::set<int> nonempty_rows;
@@ -33,21 +34,23 @@ void expand(std::vector<Pos> &galaxies) {
     // calculate the new x and y positions after expanding
     std::vector<int> expanded_x{0};
     for (int x = 1; x <= max_x; ++x) {
-        int dx = nonempty_columns.contains(x) ? 1 : 2;
+        int dx = nonempty_columns.contains(x) ? 1 : amount;
         expanded_x.push_back(expanded_x.back() + dx);
     }
 
     std::vector<int> expanded_y{0};
     for (int y = 1; y <= max_y; ++y) {
-        int dy = nonempty_rows.contains(y) ? 1 : 2;
+        int dy = nonempty_rows.contains(y) ? 1 : amount;
         expanded_y.push_back(expanded_y.back() + dy);
     }
 
     // update the galaxy positions
-    for (Pos &pos : galaxies) {
-        pos.x = expanded_x[pos.x];
-        pos.y = expanded_y[pos.y];
-    }
+    std::vector<Pos> new_galaxies(galaxies);
+    std::ranges::transform(new_galaxies, new_galaxies.begin(),
+                           [&expanded_x, &expanded_y](const Pos &pos) {
+                               return Pos{expanded_x[pos.x], expanded_y[pos.y]};
+                           });
+    return new_galaxies;
 }
 
 std::vector<Pos> read_image(std::istream &is) {
