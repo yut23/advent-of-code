7e2ef1dafce13a3b74b8c30a33b9c882ccfb13a7
yut23 <yut23@gvljohnsons.com>
2024-01-01 15:00:01 -0500

    2023: day 25 working, but very slow
    Running on the full input takes about 1 minute in fast mode, with almost
    every random stack sample pointing to the `A.contains()` calls at lines
    99 and 103 of 2023/src/day25.hpp. The find_most_tightly_connected()
    function should be replaced with a priority queue, as detailed in the
    original paper (Stoer & Wagner 1997).

---
+++ 2023/src/day25.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * File:        day25.cpp
+ *
+ * Author:      Eric T. Johnson (yut23)
+ * Created:     2023-12-26
+ *****************************************************************************/
+
+#include "day25.hpp"
+#include "lib.hpp"  // for parse_args
+#include <cassert>  // for assert
+#include <iostream> // for cout
+
+int main(int argc, char **argv) {
+    std::ifstream infile = aoc::parse_args(argc, argv);
+
+    using namespace aoc::day25;
+    Graph G = read_input(infile);
+
+    const auto &[wire_count, cut_group] =
+        MinimumCut(G, G.get_arbitrary_vertex());
+    assert(wire_count == 3);
+    std::size_t part_1 =
+        cut_group.size() * (G.num_vertices() - cut_group.size());
+    std::cout << part_1 << "\n";
+
+    return 0;
+}
---
+++ 2023/src/day25.hpp
@@ -0,0 +1,171 @@
+/******************************************************************************
+ * File:        day25.hpp
+ *
+ * Author:      Eric T. Johnson (yut23)
+ * Created:     2023-12-26
+ *****************************************************************************/
+
+#ifndef DAY25_HPP_PXSWLG7Y
+#define DAY25_HPP_PXSWLG7Y
+
+#include <cassert>       // for assert
+#include <cstddef>       // for size_t
+#include <iostream>      // for istream, cerr
+#include <limits>        // for numeric_limits
+#include <sstream>       // for istringstream
+#include <string>        // for string, getline
+#include <unordered_map> // for unordered_map
+#include <unordered_set> // for unordered_set
+#include <utility>       // for pair
+// IWYU pragma: no_include <functional>  // for hash (unordered_{set,map})
+
+namespace aoc::day25 {
+
+using vertex_t = std::string;
+using weight_t = unsigned int;
+using cut_t = std::pair<weight_t, std::unordered_set<vertex_t>>;
+
+class Graph {
+    std::unordered_map<vertex_t, std::unordered_map<vertex_t, weight_t>> edges;
+
+  public:
+    std::size_t num_vertices() const { return edges.size(); }
+    void add_edge(const vertex_t &u, const vertex_t &v, weight_t weight = 1);
+
+  private:
+    void remove_vertex(const vertex_t &u);
+
+    void merge_vertices(const vertex_t &u, const vertex_t &v);
+
+    std::pair<weight_t, vertex_t>
+    find_most_tightly_connected(const std::unordered_set<vertex_t> &A) const;
+
+  public:
+    vertex_t get_arbitrary_vertex() const { return edges.begin()->first; }
+    cut_t MinimumCutPhase(const vertex_t &a);
+};
+
+void Graph::add_edge(const vertex_t &u, const vertex_t &v, weight_t weight) {
+    assert(u != v);
+    assert(edges[u].emplace(v, weight).second);
+    assert(edges[v].emplace(u, weight).second);
+}
+
+void Graph::remove_vertex(const vertex_t &u) {
+    const auto &neighbors = edges.at(u);
+    // remove incoming edges
+    for (const auto &[v, _] : neighbors) {
+        edges.at(v).erase(u);
+    }
+    // remove outgoing edges
+    edges.erase(u);
+}
+
+void Graph::merge_vertices(const vertex_t &s, const vertex_t &t) {
+    const auto &s_neighbors = edges.at(s);
+    const auto &t_neighbors = edges.at(t);
+    vertex_t new_vertex = s + " " + t;
+
+    // add edges between new_vertex and all neighbors of s and t, excluding s
+    // and t themselves
+    for (const auto &[v, w] : s_neighbors) {
+        if (v == t) {
+            continue;
+        }
+        weight_t weight = w;
+        auto t_it = t_neighbors.find(v);
+        if (t_it != t_neighbors.end()) {
+            weight += t_it->second;
+        }
+        add_edge(new_vertex, v, weight);
+    }
+    for (const auto &[v, w] : t_neighbors) {
+        if (!s_neighbors.contains(v)) {
+            add_edge(new_vertex, v, w);
+        }
+    }
+    remove_vertex(s);
+    remove_vertex(t);
+}
+
+// Stoer-Wagner minimum cut algorithm
+
+std::pair<weight_t, vertex_t> Graph::find_most_tightly_connected(
+    const std::unordered_set<vertex_t> &A) const {
+    // find the vertex z ∉ A satisfying w(A, z) = max{w(A, y) | y ∉ A}, where
+    // w(A, y) is the sum of the weights of all edges between A and y.
+    std::pair<weight_t, vertex_t> max_vertex;
+    for (const auto &[y, neighbors] : edges) {
+        if (A.contains(y)) {
+            continue;
+        }
+        weight_t sum = 0;
+        for (const auto &[v, w] : neighbors) {
+            if (A.contains(v)) {
+                sum += w;
+            }
+        }
+        if (sum > max_vertex.first) {
+            max_vertex = {sum, y};
+        }
+    }
+    return max_vertex;
+}
+
+cut_t Graph::MinimumCutPhase(const vertex_t &a) {
+    std::unordered_set<vertex_t> A = {a};
+    vertex_t s = a, t = a;
+    weight_t cut_value = 0;
+    while (A.size() != num_vertices()) {
+        // add to A the most tightly connected vertex
+        const auto tmp = find_most_tightly_connected(A);
+        s = t;
+        t = tmp.second;
+        cut_value = tmp.first;
+        A.insert(t);
+    }
+    cut_t cut_of_the_phase{cut_value, {}};
+    std::istringstream ss{t};
+    std::string tmp;
+    while (std::getline(ss, tmp, ' ')) {
+        cut_of_the_phase.second.emplace(std::move(tmp));
+    }
+    merge_vertices(s, t);
+    return cut_of_the_phase;
+}
+
+cut_t MinimumCut(Graph G, const std::string &a) {
+    cut_t min_cut = {std::numeric_limits<weight_t>::max(), {}};
+    std::size_t total_iters = G.num_vertices() - 1;
+    for (std::size_t i = 1; i <= total_iters; ++i) {
+        cut_t cut_of_the_phase = G.MinimumCutPhase(a);
+        if (cut_of_the_phase.first < min_cut.first) {
+            min_cut = cut_of_the_phase;
+        }
+        std::cerr << "iter " << i << "/" << total_iters << " done\n";
+    }
+    assert(G.num_vertices() == 1);
+    return min_cut;
+}
+
+Graph read_input(std::istream &is) {
+    Graph G;
+    std::string line;
+    while (std::getline(is, line)) {
+        std::istringstream ss{line};
+        std::string u;
+        // read "name:"
+        ss >> u;
+        // strip trailing colon
+        u.pop_back();
+        std::string v;
+        while (ss >> v) {
+            G.add_edge(u, v);
+        }
+    }
+    return G;
+}
+
+} // namespace aoc::day25
+
+#endif /* end of include guard: DAY25_HPP_PXSWLG7Y */
