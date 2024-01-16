4f37f25d7c265616dbe78e2e0dacf60ff8fe847e
yut23 <yut23@gvljohnsons.com>
2024-01-16 12:22:25 -0500

    cpp/profile: add a variable for the binary to run

--- tools/cpp/profile.sh
+++ tools/cpp/profile.sh
@@ -11,8 +11,9 @@ num=${2:-1}
 log_file="profiling/logs/day${day}_${num}.folded"
 svg_file="profiling/day${day}_${num}.svg"
 
+BINARY=build/profile/day$day
+make_quiet "$BINARY"
 mkdir -p profiling/logs
-make_quiet "build/profile/day$day"
-perf record -g --call-graph dwarf -F 99 hyperfine --shell=none "build/profile/day$day input/day$day/input.txt"
+perf record -g --call-graph dwarf -F 99 hyperfine --shell none "$BINARY input/day$day/input.txt"
 perf script --header -F comm,pid,tid,time,event,ip,sym,dso | inferno-collapse-perf --skip-after main > "$log_file"
 inferno-flamegraph --inverted --deterministic "$log_file" > "$svg_file"
