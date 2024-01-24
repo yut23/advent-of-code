d3a3276dc454eac3ee6426f7e699fc95f0f827bf
yut23 <yut23@gvljohnsons.com>
2024-01-23 23:35:34 -0500

    2023 day 14: rewrite for better performance
    * use an unordered_map of vector<bool>s for cycle detection
    * rotate the platform after each step and always tilt in the same
      direction

--- 2023/src/day14.cpp
+++ 2023/src/day14.cpp
@@ -6,62 +6,74 @@
  *****************************************************************************/
 
 #include "day14.hpp"
-#include "lib.hpp"   // for parse_args, Pos, AbsDirection, DEBUG
-#include <algorithm> // for sort
-#include <array>     // for array
-#include <iostream>  // for cout, cerr
-#include <map>       // for map
-// IWYU pragma: no_include <functional>  // for identity, less (ranges::sort)
-// IWYU pragma: no_include <utility>  // for pair (map)
+#include "lib.hpp"       // for parse_args, DEBUG
+#include <iostream>      // for cout, cerr
+#include <unordered_map> // for unordered_map
+// IWYU pragma: no_include <functional>  // for hash (unordered_map)
 
-int main(int argc, char **argv) {
+void solve(int argc, char **argv, bool print) {
     std::ifstream infile = aoc::parse_args(argc, argv);
 
-    std::map<std::vector<aoc::Pos>, long> states;
+    std::unordered_map<std::vector<bool>, long> states;
     auto platform = aoc::day14::read_platform(infile);
-    std::ranges::sort(platform.round_rocks);
 
     if constexpr (aoc::DEBUG) {
-        std::cerr << "before tilting:\n";
-        std::cerr << platform << "\n";
+        std::cerr << "before tilting:\n" << platform << "\n";
+    }
+    // rotate so north is the initial tilt direction
+    platform.rotate_ccw();
+    if constexpr (aoc::DEBUG) {
+        std::cerr << "after rotating counter-clockwise:\n" << platform << "\n";
     }
 
     constexpr long max_step = 4'000'000'000;
-    constexpr std::array<aoc::AbsDirection, 4> directions{
-        aoc::AbsDirection::north,
-        aoc::AbsDirection::west,
-        aoc::AbsDirection::south,
-        aoc::AbsDirection::east,
-    };
 
     long cycle_length = 0;
     for (long step = 0; step < max_step; ++step) {
-        platform.tilt(directions[step % 4]);
-        if (cycle_length == 0) {
-            std::ranges::sort(platform.round_rocks);
-            auto it = states.find(platform.round_rocks);
+        platform.tilt();
+        if (cycle_length == 0 && step % 4 == 0) {
+            const std::vector<bool> round_rocks = platform.round_rocks();
+            auto it = states.find(round_rocks);
             if (it != states.end()) {
                 cycle_length = step - it->second;
+                if constexpr (aoc::DEBUG) {
+                    std::cerr << "found cycle: " << it->second << " -> " << step
+                              << "\n";
+                }
                 // skip to the final cycle iteration
-                while (step + cycle_length < max_step) {
-                    step += cycle_length;
+                step += (max_step - step) / cycle_length * cycle_length;
+                if constexpr (aoc::DEBUG) {
+                    std::cerr << "skipped to " << step << "\n";
                 }
             } else {
-                states.try_emplace(platform.round_rocks, step);
+                states.try_emplace(round_rocks, step);
             }
         }
         if (step == 0) {
             if constexpr (aoc::DEBUG) {
-                std::cerr << "after tilting:\n";
-                std::cerr << platform << "\n";
+                std::cerr << "after tilting:\n" << platform << "\n";
             }
             // part 1
-            std::cout << platform.calculate_load() << "\n";
+            int part_1 = platform.calculate_load();
+            if (print) {
+                std::cout << part_1 << "\n";
+            }
         }
+        platform.rotate_cw();
     }
 
     // part 2
-    std::cout << platform.calculate_load() << "\n";
+    int part_2 = platform.calculate_load();
+    if (print) {
+        std::cout << part_2 << "\n";
+    }
+}
 
+int main(int argc, char **argv) {
+    constexpr int N = 1;
+    // constexpr int N = 100;
+    for (int i = 0; i < N; ++i) {
+        solve(argc, argv, i == 0);
+    }
     return 0;
 }
--- 2023/src/day14.hpp
+++ 2023/src/day14.hpp
@@ -8,11 +8,12 @@
 #ifndef DAY14_HPP_A7H2MCKZ
 #define DAY14_HPP_A7H2MCKZ
 
-#include "lib.hpp"  // for Pos, Delta, AbsDirection
-#include <cstddef>  // for size_t
-#include <iostream> // for istream
-#include <string>   // for string, getline
-#include <vector>   // for vector
+#include "ds/grid.hpp" // for Grid
+#include <cstddef>     // for size_t
+#include <iostream>    // for istream
+#include <string>      // for string, getline
+#include <utility>     // for move
+#include <vector>      // for vector
 
 namespace aoc::day14 {
 
@@ -22,108 +23,138 @@ enum class Rock : char {
     cube = '#',
 };
 
+// N.B.: tilt direction is towards negative x (west in the original orientation)
 struct Platform {
-    std::vector<Pos> round_rocks;
-    std::vector<std::vector<Rock>> grid;
+    aoc::ds::Grid<Rock> rocks;
 
   private:
-    bool is_in_bounds(const Pos &pos) const {
-        return pos.x >= 0 && pos.y >= 0 &&
-               static_cast<std::size_t>(pos.y) < grid.size() &&
-               static_cast<std::size_t>(pos.x) < grid[pos.y].size();
-    }
-    bool move_rock(std::size_t index, const Delta &delta);
-    Rock &operator[](const Pos &pos) { return grid[pos.y][pos.x]; }
+    void transpose();
+    void flip_rows();
+    void flip_columns();
+
+    void roll_row_down(int row);
 
   public:
-    void add_rock(const Pos &pos, Rock rock);
-    void tilt(AbsDirection dir);
+    explicit Platform(std::vector<std::vector<Rock>> &&rocks_)
+        : rocks(std::move(rocks_)) {}
+    void rotate_cw();
+    void rotate_ccw();
+    void tilt();
     int calculate_load() const;
-    Rock operator[](const Pos &pos) const { return grid[pos.y][pos.x]; }
+    std::vector<bool> round_rocks() const;
 };
 
-bool Platform::move_rock(std::size_t index, const Delta &delta) {
-    Pos &pos = round_rocks[index];
-    Pos new_pos = pos;
-    Pos last_good_pos = pos;
-    while (true) {
-        new_pos += delta;
-        if (!is_in_bounds(new_pos)) {
-            break;
-        }
-        Rock rock = (*this)[new_pos];
-        if (rock == Rock::cube) {
-            break;
-        } else if (rock == Rock::round) {
-            // try skipping over round rocks
-            continue;
-        } else if (rock == Rock::empty) {
-            last_good_pos = new_pos;
+void Platform::transpose() {
+    const int n = rocks.width;
+    for (int y = 0; y < n; ++y) {
+        for (int x = 0; x < y; ++x) {
+            std::swap(rocks.at(x, y), rocks.at(y, x));
         }
     }
-    if (last_good_pos != pos) {
-        (*this)[pos] = Rock::empty;
-        pos = last_good_pos;
-        (*this)[pos] = Rock::round;
-        return true;
-    }
-    return false;
 }
 
-void Platform::add_rock(const Pos &pos, Rock rock) {
-    if (rock == Rock::round) {
-        round_rocks.push_back(pos);
+void Platform::flip_rows() {
+    const int n = rocks.width;
+    for (int y = 0; y < n; ++y) {
+        for (int x = 0; x < n / 2; ++x) {
+            const int z = n - x - 1;
+            std::swap(rocks.at(x, y), rocks.at(z, y));
+        }
     }
-    if (static_cast<std::size_t>(pos.y) >= grid.size()) {
-        grid.resize(pos.y + 1);
+}
+
+void Platform::flip_columns() {
+    const int n = rocks.width;
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n / 2; ++y) {
+            const int z = n - y - 1;
+            std::swap(rocks.at(x, y), rocks.at(x, z));
+        }
     }
-    if (static_cast<std::size_t>(pos.x) >= grid[pos.y].size()) {
-        for (auto &row : grid) {
-            if (static_cast<std::size_t>(pos.x) >= row.size()) {
-                row.resize(pos.x + 1, Rock::empty);
+}
+
+void Platform::rotate_cw() {
+    transpose();
+    flip_rows();
+}
+
+void Platform::rotate_ccw() {
+    transpose();
+    flip_columns();
+}
+
+void Platform::roll_row_down(int row) {
+    std::size_t i = rocks.get_index(0, row);
+    std::size_t end = i + rocks.width;
+    std::size_t last_empty = end;
+    for (; i != end; ++i) {
+        switch (rocks[i]) {
+        case Rock::round:
+            if (last_empty != end) {
+                std::swap(rocks[i], rocks[last_empty]);
+                ++last_empty;
             }
+            break;
+        case Rock::cube:
+            last_empty = end;
+            break;
+        case Rock::empty:
+            if (last_empty == end) {
+                last_empty = i;
+            }
+            break;
         }
     }
-    (*this)[pos] = rock;
 }
 
-void Platform::tilt(AbsDirection dir) {
-    Delta delta{dir, true};
-    // most naive method: bubble sort
-    for (std::size_t i = 0; i < round_rocks.size(); ++i) {
-        move_rock(i, delta);
+void Platform::tilt() {
+    for (int row = 0; row < rocks.height; ++row) {
+        roll_row_down(row);
     }
 }
 
 int Platform::calculate_load() const {
     int load = 0;
-    for (const Pos &pos : round_rocks) {
-        load += grid.size() - pos.y;
+    for (int y = 0; y < rocks.height; ++y) {
+        for (int x = 0; x < rocks.width; ++x) {
+            if (rocks.at(x, y) == Rock::round) {
+                load += rocks.width - x;
+            }
+        }
     }
     return load;
 }
 
+std::vector<bool> Platform::round_rocks() const {
+    std::vector<bool> round(rocks.data().size(), false);
+    for (std::size_t i = 0; i < rocks.data().size(); ++i) {
+        if (rocks[i] == Rock::round) {
+            round[i] = true;
+        }
+    }
+    return round;
+}
+
 std::ostream &operator<<(std::ostream &os, const Platform &platform) {
-    for (const auto &row : platform.grid) {
+    for (const auto &row : platform.rocks) {
         for (Rock rock : row) {
             os << static_cast<char>(rock);
         }
         os << "\n";
     }
-
     return os;
 }
 
 Platform read_platform(std::istream &is) {
-    Platform platform{};
+    std::vector<std::vector<Rock>> rocks;
     std::string line;
-    Pos pos;
-    for (pos.y = 0; std::getline(is, line); ++pos.y) {
-        for (pos.x = 0; pos.x < static_cast<int>(line.size()); ++pos.x) {
-            platform.add_rock(pos, Rock{line[pos.x]});
+    while (std::getline(is, line)) {
+        rocks.emplace_back();
+        for (char ch : line) {
+            rocks.back().push_back(Rock{ch});
         }
     }
-    return platform;
+    return Platform{std::move(rocks)};
 }
 
 } // namespace aoc::day14
