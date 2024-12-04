9231cb9ddd4ea0c85d71da68a73a7c997e381af5
yut23 <yut23@gvljohnsons.com>
2024-12-04 16:36:32 -0500

    2024 day 3: solve part 2

--- 2024/src/day03.cpp
+++ 2024/src/day03.cpp
@@ -8,7 +8,7 @@
 #include "day03.hpp"
 #include "lib.hpp"
 #include <fstream>  // for ifstream
-#include <iostream> // for cout
+#include <iostream> // for cout, cerr
 #include <regex>    // for regex, smatch, sregex_iterator
 #include <string>   // for string, stoi
 
@@ -17,18 +17,35 @@ int main(int argc, char **argv) {
 
     std::string memory = aoc::day03::read_input(infile);
 
-    // part 1
-    int total = 0;
+    std::regex op_regex(R"((mul)\((\d+),(\d+)\)|(do)\(\)|(don't)\(\))");
+    std::sregex_iterator op_begin{memory.begin(), memory.end(), op_regex};
+    std::sregex_iterator op_end{};
 
-    std::regex mul_regex(R"(mul\((\d+),(\d+)\))");
-    std::sregex_iterator mul_begin{memory.begin(), memory.end(), mul_regex};
-    std::sregex_iterator mul_end{};
-    for (auto i = mul_begin; i != mul_end; ++i) {
+    int total_1 = 0, total_2 = 0;
+    bool enabled = true;
+    for (auto i = op_begin; i != op_end; ++i) {
         std::smatch match = *i;
-        total += std::stoi(match[1].str()) * std::stoi(match[2].str());
+        if constexpr (aoc::DEBUG) {
+            std::cerr << "found match at " << match.position() << ": "
+                      << match.str() << "\n";
+        }
+        if (match.length(1) > 0) {
+            // mul
+            int product = std::stoi(match[2].str()) * std::stoi(match[3].str());
+            total_1 += product;
+            if (enabled) {
+                total_2 += product;
+            }
+        } else if (match.length(4) > 0) {
+            // do
+            enabled = true;
+        } else if (match.length(5) > 0) {
+            // don't
+            enabled = false;
+        }
     }
 
-    std::cout << total << "\n";
+    std::cout << total_1 << "\n" << total_2 << "\n";
 
     return 0;
 }
