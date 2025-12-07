59bcc953720fe222d08e891ac14eea501a475dcb
yut23 <yut23@gvljohnsons.com>
2025-12-07 15:59:53 -0800

    2025 day 7: solve part 2

--- 2025/src/day07.cpp
+++ 2025/src/day07.cpp
@@ -7,23 +7,26 @@
 
 #include "day07.hpp"
 #include "lib.hpp"
-#include "unit_test/pretty_print.hpp"
 #include <fstream>  // for ifstream
-#include <iostream> // for cout
+#include <iostream> // for cout, cerr
 
 int main(int argc, char **argv) {
     std::ifstream infile = aoc::parse_args(argc, argv).infile;
 
-    auto grid = aoc::day07::read_input(infile);
+    auto grid = aoc::day07::ManifoldGrid::read(infile);
 
     if constexpr (aoc::DEBUG) {
-        std::cerr << pretty_print::repr(grid) << "\n";
+        std::cerr << grid << "\n";
     }
-    int part1 = aoc::day07::propagate_tachyon_beams(grid);
+    int part1 = grid.propagate_tachyon_beams();
     if constexpr (aoc::DEBUG) {
-        std::cerr << "after propagation:\n" << pretty_print::repr(grid) << "\n";
+        std::cerr << "after propagation:\n" << grid << "\n\n";
+        if (grid.width < 20) {
+            grid.print_visits(std::cerr);
+        }
     }
     std::cout << part1 << "\n";
+    std::cout << grid.count_timelines() << "\n";
 
     return 0;
 }
--- 2025/src/day07.hpp
+++ 2025/src/day07.hpp
@@ -11,73 +11,90 @@
 #include "ds/grid.hpp" // for Grid
 #include "lib.hpp"     // for read_whole_stream
 #include <cassert>     // for assert
+#include <iomanip>     // for setw
 #include <iostream>    // for ostream, istream
+#include <iterator>    // for next
 #include <string>      // for string
-#include <utility>     // for swap
+#include <utility>     // for swap, move
 #include <vector>      // for vector
 
 namespace aoc::day07 {
-
-using aoc::ds::Grid;
 
 enum class TileType : char {
     EMPTY = '.',
     SPLITTER = '^',
     BEAM = '|',
 };
-std::ostream &operator<<(std::ostream &os, TileType tile) {
-    os << static_cast<char>(tile);
+struct Tile {
+    TileType type;
+    long visit_count = 0;
+
+    Tile &operator+=(const Tile &other);
+};
+
+Tile &Tile::operator+=(const Tile &other) {
+    if (type == TileType::EMPTY && other.type == TileType::BEAM) {
+        type = TileType::BEAM;
+    }
+    visit_count += other.visit_count;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &os, const Tile &tile) {
+    os << static_cast<char>(tile.type);
     return os;
 }
 
-using ManifoldGrid = aoc::ds::Grid<TileType>;
+class ManifoldGrid : public aoc::ds::Grid<Tile> {
+    explicit ManifoldGrid(int width, int height,
+                          aoc::ds::Grid<Tile>::container_type &&data)
+        : aoc::ds::Grid<Tile>(width, height, std::move(data)) {}
 
-ManifoldGrid read_input(std::istream &is) {
+  public:
+    static ManifoldGrid read(std::istream &is);
+    int propagate_tachyon_beams();
+    long count_timelines() const;
+    void print_visits(std::ostream &os) const;
+};
+
+ManifoldGrid ManifoldGrid::read(std::istream &is) {
     std::string data = aoc::read_whole_stream(is);
     int width = data.find_first_of('\n');
     assert(data.length() % (width + 1) == 0);
     int height = data.length() / (width + 1);
-    std::vector<TileType> transformed{};
+    std::vector<Tile> transformed{};
     transformed.reserve(height * width);
     for (char c : data) {
         if (c == 'S') {
-            transformed.push_back(TileType::BEAM);
+            transformed.push_back({TileType::BEAM, 1});
         } else if (c != '\n') {
-            transformed.push_back(TileType(c));
+            transformed.push_back({TileType(c)});
         }
     }
-    return ManifoldGrid(width, height, transformed);
+    return ManifoldGrid(width, height, std::move(transformed));
 }
 
-int propagate_tachyon_beams(ManifoldGrid &grid) {
-    ManifoldGrid::iterator prev_row_it = grid.begin();
+int ManifoldGrid::propagate_tachyon_beams() {
+    ManifoldGrid::iterator prev_row_it = begin();
     ManifoldGrid::iterator curr_row_it = prev_row_it + 1;
-    const ManifoldGrid::iterator row_end = grid.end();
+    const ManifoldGrid::iterator row_end = end();
     int split_count = 0;
     while (curr_row_it != row_end) {
         auto prev_row = *prev_row_it, curr_row = *curr_row_it;
-        for (int i = 0; i < grid.width; ++i) {
+        for (int i = 0; i < width; ++i) {
             using enum TileType;
-            TileType &curr = curr_row[i];
-            TileType &prev = prev_row[i];
-            switch (curr) {
-            case EMPTY:
-                if (prev == BEAM) {
-                    curr = BEAM;
-                }
-                break;
-            case BEAM:
-                // set by an adjacent splitter
-                break;
-            case SPLITTER:
-                if (prev == BEAM) {
-                    assert(i > 0);
-                    assert(i < grid.width - 1);
-                    curr_row[i - 1] = BEAM;
-                    curr_row[i + 1] = BEAM;
-                    ++split_count;
-                }
-                break;
+            Tile &curr = curr_row[i];
+            Tile &prev = prev_row[i];
+            if (prev.type != BEAM) {
+                continue;
+            }
+            curr += prev;
+            if (curr.type == SPLITTER) {
+                assert(i > 0);
+                assert(i < width - 1);
+                curr_row[i - 1] += prev;
+                curr_row[i + 1] += prev;
+                ++split_count;
             }
         }
 
@@ -86,6 +103,39 @@ int propagate_tachyon_beams(ManifoldGrid &grid) {
         std::swap(curr_row_it, prev_row_it);
     }
     return split_count;
+}
+
+long ManifoldGrid::count_timelines() const {
+    const auto last_row = *(std::next(this->end(), -1));
+    long count = 0;
+    for (int i = 0; i < width; ++i) {
+        count += last_row[i].visit_count;
+    }
+    return count;
+}
+
+void ManifoldGrid::print_visits(std::ostream &os) const {
+    custom_print(os, [&os](const auto tile) {
+        os << std::setw(2);
+        using enum TileType;
+        switch (tile.type) {
+        case EMPTY:
+            os << ' ';
+            break;
+        case SPLITTER:
+            os << '^';
+            break;
+        case BEAM:
+            os << tile.visit_count;
+            break;
+        }
+        os << ' ';
+    });
+}
+
+std::ostream &operator<<(std::ostream &os, const ManifoldGrid &grid) {
+    return grid.custom_print(
+        os, [&os](const auto tile) { os << static_cast<char>(tile.type); });
 }
 
 } // namespace aoc::day07
